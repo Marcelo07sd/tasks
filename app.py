094008c987ad3b9e2d5a63b22cabb78ffb60779e
Marcelo07sd <144949868+Marcelo07sd@users.noreply.github.com>
2025-03-20 22:52:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone, timedelta
 
