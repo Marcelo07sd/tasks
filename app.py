c720e12c796a5b8f2597874457d3aebe3dc79358
Marcelo07sd <144949868+Marcelo07sd@users.noreply.github.com>
2025-03-20 22:46:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,33 +42,37 @@ def home():
 
 @app.route('/create-task', methods=['POST'])
 def create():
-    task = Task(content=request.form['content'], done=False)
+    data = request.get_json()
+    task = Task(content=data['content'], done=False)
     db.session.add(task)
     db.session.commit()
-    return redirect(url_for('home'))
-
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    task = Task.query.get(id)
-    if request.method == 'POST' and task:
-        task.content = request.form['content-edit']
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('edit.html', task=task)
+    return jsonify({'id': task.id, 'content': task.content})
 
 @app.route('/done/<int:id>')
 def done(id):
-    task = Task.query.get(id)
+    task = db.session.get(Task, id)
     if task:
         task.done = not task.done
         db.session.commit()
-    return redirect(url_for('home'))
+    print(task.done)
+    return jsonify({ 'done': task.done})
+
+@app.route('/edit/<int:id>', methods=['POST', 'GET'])
+def edit(id):
+    task = db.session.get(Task, id)
+    if request.method == 'POST' and task:
+        data = request.get_json()
+        task.content = data['content_edit']
+        db.session.commit()
+        return jsonify({'id':str(id), 'content':task.content, 'result':'exito'})
+    
+    return jsonify({'id':str(id), 'content':task.content, 'result':'exito'})
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task = Task.query.get(id)
+    task = db.session.get(Task, id)
     if task:
         db.session.delete(task)
         db.session.commit()
-    return redirect(url_for('home'))
+    return jsonify({ 'id': str(id)})
 
